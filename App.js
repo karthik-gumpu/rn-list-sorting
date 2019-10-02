e8b34a60e06c9d353c83720fb4369e7910e87a4e
Karthik Gumpu <karthik.gumpu@netenrich.com>
2019-10-02 15:57:41 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import * as Colors from '@pxblue/colors';
+import { Header, ListItem, Button } from 'react-native-elements';
+import DraggableFlatList from 'react-native-draggable-flatlist';
+
+const data = [
+  {
+    name: 'George Washington',
+    value: 1789,
+  },
+  {
+    name: 'John Adams',
+    value: 1796,
+  },
+  {
+    name: 'Thomas Jefferson',
+    value: 1800,
+  },
+  {
+    name: 'James Madison',
+    value: 1808,
+  },
+  {
+    name: 'James Monore',
+    value: 1812,
+  },
+];
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: data, sortable: false };
+  }
+
+  renderDragableItem = ({ item, index, move, moveEnd, isActive }) => (
+    <TouchableOpacity onLongPress={move} onPressOut={moveEnd}>
+      <ListItem
+        title={item.name}
+        rightTitle={`${item.value}`}
+        leftIcon={{ name: 'menu' }}
+      />
+    </TouchableOpacity>
+  );
+
+  renderItem = ({ item }) => (
+    <ListItem title={item.name} rightTitle={`${item.value}`} />
+  );
+
+  toggleEdit = () => {
+    this.setState(prevState => ({
+      sortable: !prevState.sortable,
+    }));
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={Colors.blue[500]}
+          centerComponent={{
+            text: 'Sortable List',
+            style: { color: '#fff', fontSize: 16 },
+          }}
+          rightComponent={() => (
+            <Button
+              title={this.state.sortable ? 'Save' : 'Edit'}
+              type="clear"
+              onPress={this.toggleEdit}
+              titleStyle={{ color: '#fff' }}
+            />
+          )}
+        />
+        <DraggableFlatList
+          data={this.state.data}
+          renderItem={
+            this.state.sortable ? this.renderDragableItem : this.renderItem
+          }
+          keyExtractor={(item, index) => `${index}-${this.state.sortable}`}
+          scrollPercent={50}
+          onMoveEnd={({ data }) => this.setState({ data })}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
